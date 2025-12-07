8256fac9098a44cf8d511132a1dc7ecbd3317eee
BalinValery <valera.balin00@mail.ru>
2025-12-07 17:58:05 +0500

    Task3

--- Tyuiu.BalinVV.Sprint5.Task1.V18.Test/DataServiceTest.cs
+++ Tyuiu.BalinVV.Sprint5.Task1.V18.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.BalinVV.Sprint5.Task1.V18.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            string wait = @"C:\Users\Пользователь\source\repos\Tyuiu.BalinVV.Sprint5\Tyuiu.BalinVV.Sprint5.Task1.V18\bin\Debug\OutPutFileTask1.txt";
+            string wait = @"C:\Users\Пользователь\source\repos\Tyuiu.BalinVV.Sprint5\Tyuiu.BalinVV.Sprint5.Task3.V15\bin\Debug\OutPutFileTask1.txt";
 
             string path = ds.SaveToFileTextData(-5, 5);
             bool res = File.Exists(path);
---
+++ Tyuiu.BalinVV.Sprint5.Task3.V15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Tyuiu.BalinVV.Sprint5.Task3.V15.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("*******************************************************************************");
+        Console.WriteLine("* Спринт #5                                                                   *");
+        Console.WriteLine("* Тема: Базовые навыки работы в с#                                            *");
+        Console.WriteLine("* Задание #3                                                                  *");
+        Console.WriteLine("* Вариант #15                                                                 *");
+        Console.WriteLine("* Выполнил Балин В.В. | СМАРТб-25-1                                           *");
+        Console.WriteLine("*******************************************************************************");
+        Console.WriteLine("* Условие                                                                     *");
+        Console.WriteLine("* Дано выражение , вычислить его значение при x = 5, результат сохранить      *");
+        Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до трёх *");
+        Console.WriteLine("* знаков после запятой                                                        *");
+        Console.WriteLine("*******************************************************************************");
+        Console.WriteLine("* Исходные данные:                                                            *");
+        Console.WriteLine("*******************************************************************************");
+        int x = 5;
+        Console.WriteLine("* Результат:                                                                  *");
+        Console.WriteLine("*******************************************************************************");
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан");
+        Console.ReadKey();
+    }
+}
