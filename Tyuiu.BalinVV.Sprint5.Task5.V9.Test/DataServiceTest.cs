e4168a4e771f39c82cda87dc7c547164ffec7df9
BalinValery <valera.balin00@mail.ru>
2025-12-07 19:48:56 +0500

    Task5

---
+++ Tyuiu.BalinVV.Sprint5.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BalinVV.Sprint5.Task5.V9.Lib;
+using System.IO;
+namespace Tyuiu.BalinVV.Sprint5.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 2;
+            double result = Math.Round((0.7 * (Math.Pow(x, 3))) + (1.52 * (Math.Pow(x, 2))), 3);
+            double wait = 11.68;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint5.Task5.V9/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BalinVV.Sprint5.Task5.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Чтение набора данных из текстового файла                                *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнил: Балин В.В. | СМАРТб-25-1                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine(@"*Дан файл С:\DataSprint5\InPutDataFileTask5V9.txt (файл взять из архива с*");
+        Console.WriteLine(@"*огласно вашему варианту. Создать папку в ручную С:\DataSprint5\ и скопир*");
+        Console.WriteLine("*овать в неё файл) в котором есть набор значений.Найти максимальное целое *");
+        Console.WriteLine("* число в файле. Полученный результат вывести на консоль. У вещественных з*");
+        Console.WriteLine("*значений округлить до трёх знаков после запятой.                         *");
+
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+
+        string path = @"C:\DataSprint5\InPutDataFileTask5V9.txt";
+        var result = ds.LoadFromDataFile(path);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
