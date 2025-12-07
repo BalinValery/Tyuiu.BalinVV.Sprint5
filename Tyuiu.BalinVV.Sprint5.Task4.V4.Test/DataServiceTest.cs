e0ced6feddd2d89e3d1cc5a031eb36f2baae8bd0
BalinValery <valera.balin00@mail.ru>
2025-12-07 19:36:44 +0500

    Task4

---
+++ Tyuiu.BalinVV.Sprint5.Task4.V4.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Tyuiu.BalinVV.Sprint5.Task4.V4.Lib;
+namespace Tyuiu.BalinVV.Sprint5.Task4.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Пользователь\source\repos\Tyuiu.BalinVV.Sprint5\Tyuiu.BalinVV.Sprint5.Task4.V4\bin\Debug\InPutDataFileTask4V4.txt";
+            string tempPath = Path.GetTempPath();
+
+
+            string fileName = "InPutDataFileTask4V4.txt";
+            string p = Path.Combine(tempPath, fileName);
+
+            FileInfo fileInfo = new FileInfo(path);
+
+
+            bool fileExists = fileInfo.Exists;
+
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint5.Task4.V4/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Tyuiu.BalinVV.Sprint5.Task4.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5| Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #4                                                             *");
+        Console.WriteLine("* Выполнила: Балин В.В. | СМАРТб-25-1                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано выражение, вычислить его значение при x = 3, результат сохранить в *");
+        Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль.                *");
+        Console.WriteLine("* Округлить до трёх знаков после запятой                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        string path = Path.Combine(Path.GetTempPath(), "DataSprint5", "InPutDataFileTask4V4.txt");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
