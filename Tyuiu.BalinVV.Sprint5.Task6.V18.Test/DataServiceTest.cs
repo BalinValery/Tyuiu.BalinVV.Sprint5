80d5ba2ab19d61e82677d082b7f5a2ac0906b86d
BalinValery <valera.balin00@mail.ru>
2025-12-07 20:19:07 +0500

    Task6

---
+++ Tyuiu.BalinVV.Sprint5.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BalinVV.Sprint5.Task6.V18.Lib;
+namespace Tyuiu.BalinVV.Sprint5.Task6.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s = @"C:\DataSprint5\InPutDataFileTask6V18.txt";
+            FileInfo fileInfo = new FileInfo(s);
+            bool file = fileInfo.Exists;
+            Assert.AreEqual(true, file);
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint5.Task6.V18/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.BalinVV.Sprint5.Task6.V18.Lib;
+using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Обработка текствовых файлов                                       *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнил: Балин В.В. | СМАРТб-25-1                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать консольную программу на C#, которая находит количество одно-   *");
+        Console.WriteLine("* разрядных чисел в файле                                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Исходные данные: C:\\DataSprint5\\InPutDataFileTask6V18.txt              *");
+        string path = @"C:\DataSprint5\InPutDataFileTask6V18.txt";
+        Console.WriteLine("* Результат:                                                              *");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+    }
+}
